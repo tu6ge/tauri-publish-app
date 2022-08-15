e87b267c24d71fe20daaf9853e5108417578ecc1
tu6ge <772364230@qq.com>
2022-08-15 17:17:17 +0800

    feat: 选择文件夹
    改用 vue setup 方式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,47 @@
+<script setup>
+import { open } from '@tauri-apps/api/dialog'
+const collapsed = ref(false)
+const selectedKeys = ref(['1'])
+const openKeys = ref(['sub1'])
+const preOpenKeys = ref(['sub1'])
+const addAppModel = ref(false)
+const initOss = ref(true)
 
+const toggleCollapsed = () => {
+  state.collapsed = !state.collapsed;
+  state.openKeys = state.collapsed ? [] : state.preOpenKeys;
+}
+
+const columns = [
+  { title: 'Name', dataIndex: 'name', key: 'name', fixed: true },
+  { title: 'Age', dataIndex: 'age', key: 'age' },
+  { title: 'Address', dataIndex: 'address', key: 'address' },
+  { title: 'Action', key: 'action' },
+];
+
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+]
+
+function saveOssConfig() {
+  // 保存 阿里云 的配置
+}
+
+function selectAppPath() {
+  open({
+    directory: true,
+    //defaultPath: await appDir(),
+  }).then((path)=>{
+    console.log(path)
+  })
+}
+</script>
 <template>
   <div class="flex">
     <div class="slider">
@@ -66,58 +109,21 @@
 
     <a-modal v-model:visible="addAppModel" title="Basic Modal" @ok="handleOk">
       <FormKit type="form">
-        <FormKit type="text"></FormKit>
-        <FormKit type="text"></FormKit>
-        <FormKit type="text"></FormKit>
+        <FormKit type="text" label="APP 包所在目录" readonly placeholder="安装包所在目录">
+          <template #suffix>
+            <a-button type="primary" @click="selectAppPath">浏览</a-button>
+          </template>
+        </FormKit>
+        <FormKit type="text" label="APP 名称"></FormKit>
+      </FormKit>
+    </a-modal>
+    <a-modal v-model:visible="initOss" title="初始化 OSS 配置" @ok="saveOssConfig">
+      <FormKit type="form" submit-label="保存">
+        <FormKit type="text" label="KeyId" validation="required"></FormKit>
+        <FormKit type="text" label="KeySecret" validation="required"></FormKit>
+        <FormKit type="text" label="EndPoint" validation="required"></FormKit>
+        <FormKit type="text" label="Bucket" validation="required"></FormKit>
       </FormKit>
     </a-modal>
   </div>
 </template>
-<script>
-export default {
-  setup() {
-    const state = reactive({
-      collapsed: false,
-      selectedKeys: ['1'],
-      openKeys: ['sub1'],
-      preOpenKeys: ['sub1'],
-      addAppModel: false,
-    })
-
-    const toggleCollapsed = () => {
-      state.collapsed = !state.collapsed;
-      state.openKeys = state.collapsed ? [] : state.preOpenKeys;
-    }
-
-    const columns = [
-      { title: 'Name', dataIndex: 'name', key: 'name', fixed: true },
-      { title: 'Age', dataIndex: 'age', key: 'age' },
-      { title: 'Address', dataIndex: 'address', key: 'address' },
-      { title: 'Action', key: 'action' },
-    ];
-
-    const data = [
-      {
-        key: 1,
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-      },
-    ]
-
-    return {
-      ...toRefs(state),
-      toggleCollapsed,
-      columns,
-      data
-    }
-
-  },
-  data(){
-    return {
-      list:[],
-    }
-  }
-}
-</script>
