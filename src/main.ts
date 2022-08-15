adf72fc48f0d3ee4085e917956f9eb47f5388214
tu6ge <772364230@qq.com>
2022-08-15 15:19:07 +0800

    chore: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import Antd from 'ant-design-vue'
 
 import App from './App.vue'
 import 'ant-design-vue/dist/antd.css'
+import router from './router'
 
 import '@/assets/scss/global.scss' // 存放 margin padding flex 等快捷 class
 import '@/assets/scss/common.scss' // 公共样式
 
-createApp(App).use(Antd).mount('#app')
+createApp(App)
+  .use(router)
+  .use(Antd)
+  .mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "about" */ '../components/HelloWorld.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/HelloWorld.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
