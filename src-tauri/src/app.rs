a01d0ba2d5f039aa27226b670c5846a3a76ae74a
tu6ge <772364230@qq.com>
2022-08-19 15:46:09 +0800

    feat(rust): app 增删改查

---
+++ src-tauri/src/app.rs
@@ -0,0 +1,96 @@
+use std::{fs::File, io::{Write, Read}};
+
+use serde::{Deserialize, Serialize};
+use tauri::{api::path::data_dir};
+
+
+#[derive(Default,Serialize, Deserialize, Clone)]
+pub struct AppConfig{
+  name: String,
+  path: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct AppList{
+  list: Vec<AppConfig>,
+}
+
+impl AppList {
+  pub fn get_all()-> Result<Self, String>{
+    let mut content = String::new();
+    let data_dir = data_dir();
+
+    let mut file = File::open(
+      data_dir.unwrap()
+      .join("tauri_oss_app_config.json")
+    )
+    .map_err(|_|"open tauri_oss_app_config.json failed".to_string())?;
+
+    file.read_to_string(&mut content).map_err(|_|"reading tauri_oss_app_config.json failed".to_string())?;
+
+    if content.is_empty() {
+      return Ok(AppList {
+        list: Vec::new(),
+      });
+    }
+
+    let list: AppList = serde_json::from_str(&content).map_err(|_|"json parse failed".to_string())?;
+
+    Ok(list)
+  }
+
+  fn save(&self) -> Result<String, String> {
+    let json = serde_json::to_string_pretty(self).map_err(|_|{"转 json 格式失败".to_string()})?;
+    let data_dir = data_dir();
+    if let None = data_dir {
+      return Err("找不到存储路径".into());
+    }
+
+    let mut file = File::create(
+        data_dir.unwrap()
+        .join("tauri_oss_app_config.json")
+      )
+      .map_err(|_|"create tauri_oss_app_config.json failed".to_string())?;
+    file.write_fmt(format_args!("{}", json)).map_err(|_|"writing tauri_oss_app_config.json failed".to_string())?;
+
+    Ok("ok".into())
+  }
+
+  fn push(mut self, app: AppConfig)-> Self{
+    self.list.push(app);
+    self
+  }
+
+  fn update(mut self, index: usize, app: AppConfig)-> Self{
+    self.list[index] = app;
+    self
+  }
+
+  fn remove(mut self, index: usize)-> Self{
+    self.list.remove(index);
+    self
+  }
+}
+
+#[tauri::command]
+pub fn get_all_app(config: AppConfig) -> Result<AppList, String> {
+  AppList::get_all()
+}
+
+#[tauri::command]
+pub fn update_app(index: usize, config: AppConfig) -> Result<String, String> {
+  AppList::get_all()?.update(index, config).save()
+}
+
+
+#[tauri::command]
+pub fn push_app(config: AppConfig) -> Result<String, String> {
+  let list = AppList::get_all()?;
+  list.push(config).save()
+}
+
+#[tauri::command]
+pub fn remove_app(index: usize) -> Result<String, String> {
+  let list = AppList::get_all()?;
+  list.remove(index).save()
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,11 +6,21 @@
 mod oss;
 use oss::{OssConfigWrapper,save_oss_config,get_oss_config};
 
+mod app;
+use app::{get_all_app, push_app, remove_app, update_app};
+
 fn main() {
     let ctx = tauri::generate_context!();
     tauri::Builder::default()
         .manage(OssConfigWrapper::default())
-        .invoke_handler(tauri::generate_handler![save_oss_config,get_oss_config])
+        .invoke_handler(tauri::generate_handler![
+            save_oss_config,
+            get_oss_config,
+            get_all_app,
+            push_app,
+            remove_app,
+            update_app,
+        ])
         .menu(
             tauri::Menu::os_default("Tauri Vue Template")
         )
